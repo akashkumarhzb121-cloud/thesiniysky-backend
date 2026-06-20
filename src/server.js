abe968d4b55fa017ad7b3cc65620c588f64ba069
Akashkumarr <akashkumarhzb121@gmail.com>
2026-06-20 19:55:21 +0530

    Add styled root route with API info

--- src/server.js
+++ src/server.js
@@ -117,3 +117,4 @@ io.on('connection', (socket) => { socket.on('disconnect', () => {}); });
 
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log('Server on port', PORT));
+
